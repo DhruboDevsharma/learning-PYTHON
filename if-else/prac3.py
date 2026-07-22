c92aabe20836ad99bb64167e0be2b13638a70cd1
DhruboDevsharma <dhrubodevsharma@gmail.com>
2026-07-23 01:04:22 +0530

    Write a program that takes a number from the user and prints

---
+++ if-else/prac3.py
@@ -0,0 +1,8 @@
+# Write a program that takes a number from the user and prints "Even" if it is even, otherwise "Odd".
+
+num = int(input())
+
+if(num%2 == 0):
+    print("even")
+else:
+    print("Odd")
