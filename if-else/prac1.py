ec958f6d18818588d57650da2968e169a9a93b0c
DhruboDevsharma <dhrubodevsharma@gmail.com>
2026-07-23 00:58:33 +0530

    # write a program for check number is negative, positive or zero

---
+++ if-else/prac1.py
@@ -0,0 +1,10 @@
+# write a program for check number is negative, positive or zero
+
+num = int(input())
+
+if( num >0):
+    print("Positive")
+elif(num<0):
+    print("Negative")
+else:
+    print("Zero")
